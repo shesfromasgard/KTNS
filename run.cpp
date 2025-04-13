0050e30ec060ef90b8b74cc87c6e3df2779093df
laura🍧 <shesfromasgard@gmail.com>
2025-04-12 23:01:40 -0300

    Adaptação para receber diversos arquivos de instâncias do SSP padrão

--- run.cpp
+++ run.cpp
@@ -2,8 +2,11 @@
 #include <vector>
 #include <fstream>
 #include <numeric>
+#include <string>
+#include <filesystem>
 
 using namespace std;
+namespace fs = std::filesystem;
 
 unsigned int m, n, c;
 
@@ -19,75 +22,91 @@ vector<vector<int>> matrix;
 vector<int> toolLife;
 vector<int> executionTime;
 
-ifstream input("input.txt");
-ofstream output("output.txt");
-
 long KTNS(const vector<int> processos, bool debug);
 
-int main() {
-
-
-    if(!input.is_open()) {
-        cerr << "Erro ao abrir o arquivo de entrada." << endl;
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        cerr << "Uso: " << argv[0] << " <diretorio_entrada> <diretorio_saida>" << endl;
         return 1;
     }
 
-    if(!output.is_open()) {
-        cerr << "Erro ao abrir o arquivo de saída." << endl;
+    fs::path input_dir = argv[1];
+    fs::path output_dir = argv[2];
+
+    if (!fs::is_directory(input_dir)) {
+        cerr << "Erro: Diretório de entrada '" << input_dir << "' não encontrado." << endl;
         return 1;
     }
 
-    input >> m >> n >> c;
-
-    matrix.resize(m, vector<int>(n));
-    toolLife.resize(m);
-    executionTime.resize(n);
-
-    for(int i = 0; i < m; ++i) {
-        input >> toolLife[i];
-        output << toolLife[i] << " ";
+    if (!fs::is_directory(output_dir)) {
+        cerr << "Erro: Diretório de saída '" << output_dir << "' não encontrado." << endl;
+        return 1;
     }
 
-    output << endl;
+    for (const auto& entry : fs::directory_iterator(input_dir)) {
+        if (entry.is_regular_file() && entry.path().extension() == ".txt") {
+            fs::path input_filepath = entry.path();
+            fs::path filename = input_filepath.filename();
+            fs::path base_name = filename.stem();
+            fs::path output_filename = base_name.string() + "_output.txt";
+            fs::path output_filepath = output_dir / output_filename;
 
-    for(int i = 0; i < n; ++i) {
-        input >> executionTime[i];
-        output << executionTime[i] << " ";
+            ifstream input(input_filepath);
+            ofstream output(output_filepath);
+
+            if (!input.is_open()) {
+                cerr << "Erro ao abrir o arquivo de entrada: " << input_filepath << endl;
+                continue; // Ir para o próximo arquivo
+            }
+
+            if (!output.is_open()) {
+                cerr << "Erro ao abrir o arquivo de saída: " << output_filepath << endl;
+                input.close();
+                continue; // Ir para o próximo arquivo
+            }
+
+            input >> m >> n >> c;
+
+            matrix.resize(m, vector<int>(n));
+            toolLife.resize(m);
+            executionTime.resize(n);
+
+            for (int j = 0; j < m; ++j)
+                for (int k = 0; k < n; ++k)
+                    input >> matrix[j][k];
+
+            vector<int> processos(n);
+            iota(processos.begin(), processos.end(), 0);
+
+            output << KTNS(processos, false) << endl;
+
+            input.close();
+            output.close();
+
+            cout << "Arquivo '" << filename << "' processado. Saída em '" << output_filename << "'" << endl;
+        }
     }
 
-    output << endl;
-
-    for(int i = 0; i < m; ++i)
-        for(int j = 0; j < n; ++j)
-            input >> matrix[i][j];
-
-    output << "Deu super certo!" << endl;
-
-    vector<int> processos(n);
-
-    iota(processos.begin(), processos.end(), 0);
-
-    output << KTNS(processos, true);
+    cout << "Processamento concluído." << endl;
 
     return 0;
 }
 
 long KTNS(const vector<int> processos, bool debug = false) {
-
-    if(processos.size() == 0) {
+    if (processos.size() == 0) {
         return 0;
     }
 
     vector<int> loaded(m, 0);
     int u = 0; // Ferramentas no magazine
 
-    int priorities[m][processos.size()];
-    int magazine[m][processos.size()];
+    vector<vector<int>> priorities(m, vector<int>(processos.size()));
+    vector<vector<int>> magazine(m, vector<int>(processos.size()));
 
-	if (debug) {
+    if (debug) {
         cout << std::endl << "Matriz de Ferramentas no KTNS" << std::endl;
-        for (unsigned j = 0; j < m; ++j){
-            for (unsigned i = 0; i < n; ++i){
+        for (unsigned j = 0; j < m; ++j) {
+            for (unsigned i = 0; i < n; ++i) {
                 cout << matrix[j][i] << " ";
             }
             cout << endl;
@@ -103,57 +122,56 @@ long KTNS(const vector<int> processos, bool debug = false) {
 
     cout << "Magazine" << endl << endl;
 
-    for(unsigned j = 0; j < m; ++j) {
+    for (unsigned j = 0; j < m; ++j) {
         loaded[j] = matrix[j][processos[0]];
-        if(matrix[j][processos[0]] == 1)
+        if (matrix[j][processos[0]] == 1)
             ++u;
-        for(unsigned i = 0; i < processos.size(); ++i) {
+        for (unsigned i = 0; i < processos.size(); ++i) {
             magazine[j][i] = matrix[j][processos[i]];
-            if(debug)
+            if (debug)
                 cout << magazine[j][i] << " ";
         }
-        if(debug) {
+        if (debug) {
             cout << endl;
         }
     }
 
     cout << endl << endl;
 
-    for (unsigned i = 0; i < m; ++i){
-		for (unsigned j = 0; j < processos.size(); ++j){
-			if (magazine[i][j] == 1)
-				priorities[i][j] = 0;
-			else {
-				int proxima = 0;
-				bool usa = false;
-				for (unsigned k= j + 1; k< processos.size(); ++k){
-					++proxima;
-					if (magazine[i][k] == 1){
-						usa = true;
-						break;
-					}
-				}
-				if (usa)
-					priorities[i][j]=proxima;
-				else
-					priorities[i][j]=-1;
-			}
-		}
-	}
+    for (unsigned i = 0; i < m; ++i) {
+        for (unsigned j = 0; j < processos.size(); ++j) {
+            if (magazine[i][j] == 1)
+                priorities[i][j] = 0;
+            else {
+                int proxima = 0;
+                bool usa = false;
+                for (unsigned k = j + 1; k < processos.size(); ++k) {
+                    ++proxima;
+                    if (magazine[i][k] == 1) {
+                        usa = true;
+                        break;
+                    }
+                }
+                if (usa)
+                    priorities[i][j] = proxima;
+                else
+                    priorities[i][j] = -1;
+            }
+        }
+    }
 
     if (debug) {
-
         for (unsigned j = 0; j < m; ++j) {
             for (unsigned i = 0; i < processos.size(); ++i) {
-                    cout << priorities[j][i] << " ";
-                }
-                cout << endl;
+                cout << priorities[j][i] << " ";
+            }
+            cout << endl;
         }
-    
+
         cout << "Ferramentas carregadas: " << endl;
-        for (unsigned j=0; j < m; j++) {
+        for (unsigned j = 0; j < m; j++) {
             if (loaded[j] == 33) exit(0);
-                    cout << loaded[j] << endl;
+            cout << loaded[j] << endl;
         }
     }
 
@@ -164,52 +182,52 @@ long KTNS(const vector<int> processos, bool debug = false) {
     }
 
     int trocas = 0;
-	for (unsigned i = 1; i<processos.size(); ++i) {
-		for (unsigned j = 0; j < m; ++j){
-			if ((magazine[j][i] == 1) && (loaded[j] == 0)){
-				loaded[j] = 1;
-				++u;
-			}
-		}
-		if (debug) {
-			cout << u << " Ferramentas carregadas" << endl;
-		}
-		while (u > c){
-			int maior = 0;
-			int pMaior = -1;
-			for (unsigned j=0; j < m; ++j) {
-				if (magazine[j][i] != 1){ // Ferramenta não utilizada pelo processo atual
-					if ((loaded[j] == 1) && (priorities[j][i] == -1)) { // Essa ferramenta não será mais utilizada e é um excelente candidato a remoção
-						pMaior = j;
-						break;
-					} else {
-						if ((priorities[j][i] > maior) && loaded[j] == 1) {
-							maior = priorities[j][i];
-							pMaior = j;
-						}
-					}
-				}
-			}
-			loaded[pMaior] = 0;
-			if (debug) {
-				cout << "Retirou " << i << ":" << pMaior << endl;
-			}
-			--u;
-			++trocas;
-			if (debug) {
-				cout << trocas << " trocas " << endl;
-			}
-		}
-		if (debug) {
+    for (unsigned i = 1; i < processos.size(); ++i) {
+        for (unsigned j = 0; j < m; ++j) {
+            if ((magazine[j][i] == 1) && (loaded[j] == 0)) {
+                loaded[j] = 1;
+                ++u;
+            }
+        }
+        if (debug) {
+            cout << u << " Ferramentas carregadas" << endl;
+        }
+        while (u > c) {
+            int maior = 0;
+            int pMaior = -1;
+            for (unsigned j = 0; j < m; ++j) {
+                if (magazine[j][i] != 1) { // Ferramenta não utilizada pelo processo atual
+                    if ((loaded[j] == 1) && (priorities[j][i] == -1)) { // Essa ferramenta não será mais utilizada e é um excelente candidato a remoção
+                        pMaior = j;
+                        break;
+                    } else {
+                        if ((priorities[j][i] > maior) && loaded[j] == 1) {
+                            maior = priorities[j][i];
+                            pMaior = j;
+                        }
+                    }
+                }
+            }
+            loaded[pMaior] = 0;
+            if (debug) {
+                cout << "Retirou " << i << ":" << pMaior << endl;
+            }
+            --u;
+            ++trocas;
+            if (debug) {
+                cout << trocas << " trocas " << endl;
+            }
+        }
+        if (debug) {
             cout << "Ferramentas carregadas: " << endl;
-            for (unsigned j=0; j < m; ++j) {
+            for (unsigned j = 0; j < m; ++j) {
                 cout << loaded[j] << endl;
             }
-	    }
-	}
-	if (debug) {
-	    cout << ": " << trocas << "trocas" << endl;
-	}
+        }
+    }
+    if (debug) {
+        cout << ": " << trocas << "trocas" << endl;
+    }
 
     return trocas + c;
 }
